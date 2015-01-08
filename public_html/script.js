f3fa2088f57fe486c9bdd0280e445fcd61e69bdb
Roger Pirrone <r.j.pirrone.4@gmail.com>
2015-01-08 11:27:09 -0800

    Made mouse moving over smiley face picture change text.

--- public_html/script.js
+++ public_html/script.js
@@ -3,4 +3,17 @@ $("document").ready(function(){
    
    $('p:last').css({"background-color": "purple", "color": "white"});
    
+   $('#face').bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
+   
+   
 });
+
+function mouseOverMe(){
+    console.log("mouseover");
+    $("#alter").html("You put the mouse over the smiley face.");
+}
+
+function mouseOutMe(){
+    console.log("mouseout");
+    $("#alter").html("You do not have the mouse over the smiley face.");
+}
